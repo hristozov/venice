e58a7520bc08dcb58e5d3d340b8a674c64a8a9d7
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-29 18:12:35 +0300

    Modifications to "bubble".

--- src/venice/sort/bubble.clj
+++ src/venice/sort/bubble.clj
@@ -1,5 +1,26 @@
 (ns venice.sort.bubble
   (:gen-class))
 
-(defn swap-front [[a b & rest]] (concat [b] [a] rest))
+(defn swap-front [[a b & rest]]
+  (concat [b] [a] rest))
+
+(defn process-list [l]
+  (if (< (count l) 2)
+    l
+    ((fn [f s & rest]
+       (let [smaller (min f s)]
+         [bigger (max f s)]
+         (concat [smaller] [bigger] rest))))))
+
+(defn drop-last [l]
+  (if (empty? l)
+    l
+    (take (- (count l) 1) l)))
+
+(defn bubble [l]
+  (if (empty? l)
+    l
+    (let [l-without-last (take (- (count l) 2) l)]
+      [last-element (nth (- (count l) 1) l)])
+    wtf))
 
