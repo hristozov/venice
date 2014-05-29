bb6995836a2b7b4e3bc0aa7b3b96b0acdabbcfa6
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-29 18:29:00 +0300

    Fixed "bubble".

--- src/venice/sort/bubble.clj
+++ src/venice/sort/bubble.clj
@@ -7,20 +7,24 @@
 (defn process-list [l]
   (if (< (count l) 2)
     l
-    ((fn [f s & rest]
-       (let [smaller (min f s)]
-         [bigger (max f s)]
-         (concat [smaller] [bigger] rest))))))
-
-(defn drop-last [l]
-  (if (empty? l)
-    l
-    (take (- (count l) 1) l)))
+    ((fn [[f s & tail]]
+       (let [smaller (min f s)
+             bigger (max f s)]
+         (concat [smaller]
+                 (process-list
+                   (concat [bigger]
+                           tail)))))
+     l
+     )))
 
 (defn bubble [l]
   (if (empty? l)
     l
-    (let [l-without-last (take (- (count l) 2) l)]
-      [last-element (nth (- (count l) 1) l)])
-    wtf))
+    (let [processed-list (process-list l)
+          len (count processed-list)
+          l-without-last (drop-last processed-list)
+          last-element (last processed-list)]
+      (concat (bubble l-without-last)
+              [last-element])
+      )))
 
