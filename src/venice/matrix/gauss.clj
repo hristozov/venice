1b094ddd95dc3fadd175b8bd1bf9d75de6651fd1
Georgi Hristozov <georgi@forkbomb.nl>
2014-06-16 19:39:40 +0300

    WIP

---
+++ src/venice/matrix/gauss.clj
@@ -0,0 +1,22 @@
+(ns venice.matrix.gauss
+  (:gen-class))
+
+(defn- swap [v i1 i2] 
+  (assoc v i2 (v i1) i1 (v i2)))
+
+(defn- column-pivot
+  [m i]
+  (println "ccc" m i)
+  (let [column (map #(nth % i) m)]
+    (first (apply max-key
+                  #(Math/abs (second %))
+                  (map-indexed vector column)))))
+
+(defn gauss
+  [m]
+  (let [number-of-rows (count m)
+        number-of-columns (count (first m))]
+    (loop [column-index 0
+           matrix m]
+      (let [pivot (column-pivot matrix column-index)]
+        (println column-index pivot)))))
