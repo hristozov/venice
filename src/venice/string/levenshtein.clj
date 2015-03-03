1c20a9fd2c0ef0661f92ee6a245bdb46aaecb2f3
Georgi Hristozov <georgi@forkbomb.nl>
2015-03-03 21:23:15 +0200

    Use "letfn" instead of "defn-".

--- src/venice/string/levenshtein.clj
+++ src/venice/string/levenshtein.clj
@@ -1,23 +1,21 @@
 (ns venice.string.levenshtein
   (:gen-class))
 
-(defn- indicator [s1 s2 i j] 
-  (if (= (.charAt s1 (- i 1))
-         (.charAt s2 (- j 1)))
-    0
-    1))
-
-(defn- helper [s1 s2 i j]
-  (if (= 0 (min i j))
-    (max i j)
-    (min (+ (helper s1 s2 (- i 1) j) 
-            1)
-         (+ (helper s1 s2 i (- j 1)) 
-            1)
-         (+ (helper s1 s2 (- i 1) (- j 1))
-            (indicator s1 s2 i j)))))
-
 (defn levenshtein 
   "Calculates Levenshtein distance"
   [s1 s2]
-  (helper s1 s2 (.length s1) (.length s2)))
+  (letfn [(indicator [i j] 
+                     (if (= (.charAt s1 (- i 1))
+                            (.charAt s2 (- j 1)))
+                       0
+                       1))
+          (helper [i j]
+                  (if (= 0 (min i j))
+                    (max i j)
+                    (min (+ (helper (- i 1) j) 
+                            1)
+                         (+ (helper i (- j 1)) 
+                            1)
+                         (+ (helper (- i 1) (- j 1))
+                            (indicator i j)))))]
+          (helper (.length s1) (.length s2))))
